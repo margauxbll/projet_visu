fca240680acf2f7a8147fb5ae031c05569003e59
Clémence CHESNAIS <clem.chesnais@outlook.fr>
2023-04-12 21:51:59 +0200

    modif clem

--- margaux.py
+++ margaux.py
@@ -1,10 +1,12 @@
 import pandas as pd
 from bokeh.plotting import figure, output_file, show, ColumnDataSource
 from bokeh.models import HoverTool,Tabs, TabPanel
+from bokeh.palettes import Turbo256
+from bokeh.transform import linear_cmap, factor_cmap
 import numpy as np
 from pprint import pprint
-from shapely.geometry import MultiPolygon
-import geopandas as gpd
+# from shapely.geometry import MultiPolygon
+# import geopandas as gpd
 
 eolien = pd.read_csv("data/installations-de-production-de-la-filiere-eolien-par-commune.csv", sep = ';')
 # print(eolien)
@@ -16,7 +18,7 @@ solaire = pd.read_csv("data/installations-de-production-de-la-filiere-solaire-pa
 # print(solaire)
 
 reserves_naturelles = pd.read_csv("data/reserves-naturelles-regionales-de-bretagne.csv", sep = ';')
-print(reserves_naturelles)
+# print(reserves_naturelles["Geo Shape"])
 
 parcs_naturels = pd.read_csv("data/parcs-naturels-regionaux-actifs-et-en-projet-de-bretagne.csv", sep = ';')
 # print(parcs_naturels)
@@ -24,14 +26,64 @@ parcs_naturels = pd.read_csv("data/parcs-naturels-regionaux-actifs-et-en-projet-
 consommation = pd.read_csv("data/consommation-annuelle-delectricite-et-gaz-par-commune-et-par-code-naf.csv", sep = ';')
 # print(consommation)
 
-source1 = ColumnDataSource(reserves_naturelles)
+# Création d'un fichier json normalisé 
+#Première structure : dico qui à chaque nom de commune associe un dictionnaire
 
-coordinates = reserves_naturelles["Geo Shape"]["coordinates"][0][0]
+
+# dicoCom = {}
+
+# for reserve in reserves_naturelles:
+
+#     #Est-ce qu'on voit la commune pour la première fois ?
+#     nom = reserve["nom_commune"]
+    
+#         macom = {}
+#         macom["commune"] = nom
+#         macom["photo"]= prod.get("nb_sites_photovoltaique_enedis",0)
+#         macom["bio"]= prod.get("nb_sites_bio_energie_enedis",0)
+#         macom["hydrau"]= prod.get("nb_sites_hydraulique_enedis",0)
+#         macom["cogen"] = prod.get("nb_sites_cogeneration_enedis",0)
+
+#         #Récupération des coordonnées 2D
+#         coords = prod["centroid"]
+#         X, Y = coor_wgs84_to_web_mercator(coords["lon"], coords["lat"])
+#         macom["pointx"] = X
+#         macom["pointy"] = Y
+
+#         #Récupération des coordonnées de zone
+#         zone = prod["geom"]["geometry"]["coordinates"][0][0]
+#         coord = [coor_wgs84_to_web_mercator(c[0], c[1]) for c in zone]
+#         macom["zonex"]=[c[0] for c in coord]
+#         macom["zoney"]=[c[1] for c in coord]
+
+#         dicoCom[nom] = macom
+
+
+#     else :
+#         macom = dicoCom[nom]
+#         macom["photo"]=macom["photo"]+ prod.get("nb_sites_photovoltaique_enedis",0)
+#         macom["bio"] = macom["bio"]+prod.get("nb_sites_bio_energie_enedis", 0)
+#         macom["hydrau"] = macom["hydrau"]+prod.get("nb_sites_hydraulique_enedis", 0)
+#         macom["cogen"] = macom["cogen"]+prod.get("nb_sites_cogeneration_enedis", 0)
+
+# final = list(dicoCom.values())
+# print(final)
+
+# with open("TD5/production_normalise.json", "w",encoding='utf-8') as jsonFile:
+#     jsonFile.write(json.dumps(final, indent = 4))
+
+
+
+
+# Onglet 3 : les réserves naturelles
+
+
 
 #Création de la figure avec arrière plan
-p = figure(x_axis_type="mercator", y_axis_type="mercator", active_scroll="wheel_zoom", title="Les réserves naturelles en Bretagne")
-p.add_tile("CartoDb Positron")
-# p.patches(poly, color='blue', alpha=0.5, source=source1)
+# nom = reserves_naturelles["nom_long"].unique()
+# p = figure(x_axis_type="mercator", y_axis_type="mercator", active_scroll="wheel_zoom", title="Les réserves naturelles en Bretagne")
+# p.add_tile("CartoDb Positron")
+# p.patches(xs="longitude",ys="latitude",source =source,alpha = 0.5,color = factor_cmap('nom_long', palette=Turbo256,factors = nom))
 # show(p)
 
 
