aa4727e2797e376ed75326b1bcca5ddd369265ab
margauxbll <margaux.bailleul@free.fr>
2023-04-23 17:07:01 +0200

    ajout graphique annees

--- margaux.py
+++ margaux.py
@@ -66,7 +66,7 @@ count_by_secteur = conso_secteur.size()
 df_nb_secteur = count_by_secteur.reset_index(name='nombre_elements') # transformation en data.frame
 df_nb_secteur.columns = ["Secteur","Nb"] #Renommage des colonnes
 donnees_nb_secteur = ColumnDataSource(df_nb_secteur)
-print(df_nb_secteur)
+# print(df_nb_secteur)
 
 # data = pd.Series(df_nb_secteur).reset_index(name='Nb')
 df_nb_secteur['angle'] = df_nb_secteur['Nb']/df_nb_secteur['Nb'].sum() * 2*pi
@@ -83,7 +83,7 @@ pie.axis.axis_label = None
 pie.axis.visible = False
 pie.grid.grid_line_color = None
 
-show(pie)
+# show(pie)
 
 # Somme des opérateurs (Enedis le plus utilisé)
 
@@ -107,7 +107,7 @@ picker1.js_link('color', barre.glyph, 'line_color')
 
 # Afficher le graphique dans le notebook
 layout = row(p, column(picker1))
-show(layout)
+# show(layout)
 
 columns = [
         TableColumn(field="Opérateur", title="Opérateur"),
@@ -237,4 +237,69 @@ legend.title = "Cliquez sur une légende pour la masquer"
 
 # show(carte)
 
+####### graphique vbar ######
 
+conso_op_annee = consommation.groupby(['Opérateur', 'Année']) #Compter 
+count_by_conso_annee = conso_op_annee.size()
+df_nb_conso_annee = count_by_conso_annee.reset_index(name='nombre_elements') # transformation en data.frame
+df_nb_conso_annee.columns = ["Opérateur", "Année","Nb"] #Renommage des colonnes
+df_nb_conso_annee = df_nb_conso_annee[(df_nb_conso_annee['Opérateur'] == 'Enedis') | (df_nb_conso_annee['Opérateur'] == 'GRDF') ]
+print(df_nb_conso_annee)
+df_nb_conso_annee['Année'] = df_nb_conso_annee["Année"].astype(str)
+# donnees_nb_conso_annee = ColumnDataSource(df_nb_conso_annee)
+# -- Ajouter les barres
+# p5 = figure(x_range = df_nb_conso['Opérateur'].unique(), title="Nombre d'opérateurs")
+# p5.vbar(x='Opérateur', top='Nb', width=0.9, source=donnees_nb_conso)
+# source = ColumnDataSource(data=donnees_nb_conso_annee)
+
+from bokeh.models import ColumnDataSource, FactorRange
+from bokeh.plotting import figure, show
+
+# this creates [ ("Apples", "2015"), ("Apples", "2016"), ("Apples", "2017"), ("Pears", "2015), ... ]
+x = [ (annee, op) for op in df_nb_conso_annee['Opérateur'].unique() for annee in df_nb_conso_annee['Année'].unique() ]
+print(x)
+counts = tuple(df_nb_conso_annee["Nb"])
+print(counts)
+source = ColumnDataSource(data=dict(x=x, counts=counts))
+
+p = figure(x_range=FactorRange(*x), height=350, title="Fruit Counts by Year",
+           toolbar_location=None, tools="")
+
+p.vbar(x='x', top='counts', width=0.9, source=source)
+
+p.y_range.start = 0
+p.x_range.range_padding = 0.1
+p.xaxis.major_label_orientation = 1
+p.xgrid.grid_line_color = None
+
+show(p)
+
+# fruits = ['Apples', 'Pears', 'Nectarines', 'Plums', 'Grapes', 'Strawberries']
+# years = ['2015', '2016', '2017']
+
+# data = {'fruits' : fruits,
+#         '2015'   : [2, 1, 4, 3, 2, 4],
+#         '2016'   : [5, 3, 3, 2, 4, 6],
+#         '2017'   : [3, 2, 4, 4, 5, 3]}
+
+# palette = ["#c9d9d3", "#718dbf", "#e84d60"]
+
+# this creates [ ("Apples", "2015"), ("Apples", "2016"), ("Apples", "2017"), ("Pears", "2015), ... ]
+# x = [ (fruit, year) for fruit in fruits for year in years ]
+# print(x)
+# counts = sum(zip(data['2015'], data['2016'], data['2017']), ()) # like an hstack
+# print(counts)
+# source = ColumnDataSource(data=dict(x=x, counts=counts))
+
+# p = figure(x_range=FactorRange(*x), height=350, title="Fruit Counts by Year",
+#            toolbar_location=None, tools="")
+
+# p.vbar(x='x', top='counts', width=0.9, source=source, line_color="white",
+#        fill_color=factor_cmap('x', palette=palette, factors=years, start=1, end=2))
+
+# p.y_range.start = 0
+# p.x_range.range_padding = 0.1
+# p.xaxis.major_label_orientation = 1
+# p.xgrid.grid_line_color = None
+
+# show(p)
